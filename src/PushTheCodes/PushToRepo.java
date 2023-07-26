111f243c3f229636a3fe18560243e0cdcb23f621
madmaax47 <126536626+madmaax47@users.noreply.github.com>
2023-07-26 18:56:20 -0400

    Update PushToRepo.java
    some changes on remote repositories

--- src/PushTheCodes/PushToRepo.java
+++ src/PushTheCodes/PushToRepo.java
@@ -6,5 +6,17 @@ public class PushToRepo {
 		// TODO Auto-generated method stub
 
 	}
+	public void pullMethod(){
+		int a = 8;
+		int b =9;
+		if(a > b){
+		System.out.println("a is bigger than b");
+		}else{
+			System.out.println("a is smaller than b");
+
+		}
+		
+
+	}
 
 }
