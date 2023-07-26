fe3ae1c6dbf6a830be6cdc147cc85910affa55b2
madmaax47 <taha.big@tekschool.us>
2023-07-26 18:30:51 -0400

    this is commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 4hmad
+ *
+ */
+module practice_purpose {
+}
---
+++ src/practice_purpose/practice.java
@@ -0,0 +1,10 @@
+package practice_purpose;
+
+public class practice {
+
+	public static void main(String[] args) {
+		System.out.println("hi there");
+
+	}
+
+}
